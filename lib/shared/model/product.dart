9c4eb37d126f76fedef24d387ce5985d425ce4f0
DESKTOP-Q2VSK4R\MANH <trantrantrantrantran726@gmail.com>
2023-05-19 08:54:54 +0700

    add product in cart on database

--- lib/shared/model/product.dart
+++ lib/shared/model/product.dart
@@ -1,15 +1,15 @@
 class Product {
   String id;
   String name;
-  String price;
+  double price;
   String thumbnail;
-
-  Product({
-    required this.id,
-    required this.name,
-    required this.price,
-    required this.thumbnail,
-  });
+  int? quantity;
+  Product(
+      {required this.id,
+      required this.name,
+      required this.price,
+      required this.thumbnail,
+      this.quantity});
 
   static List<Product> parseProductList(map) {
     var list = map['data'] as List<dynamic>;
@@ -17,11 +17,18 @@ class Product {
   }
 
   factory Product.fromJson(Map<String, dynamic> json) => Product(
-      id: json['id'].toString(),
-      name: json["name"].toString(),
-      price: json['price'].toString(),
-      thumbnail: json["thumbnail"].toString());
+        id: json['id'].toString(),
+        name: json["name"].toString(),
+        price: double.parse(json["price"].toString()),
+        thumbnail: json["thumbnail"].toString(),
+        quantity: int.parse(json["quantity"].toString()),
+      );
 
-  Map<String, dynamic> toJson() =>
-      {"id": id, "name": name, "price": price, "thumbnail": thumbnail};
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "price": price,
+        "thumbnail": thumbnail,
+        'quantity': quantity
+      };
 }
